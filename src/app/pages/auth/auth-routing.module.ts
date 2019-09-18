01f0c271a9cc0ec4cb0f94f3149837a707b2cf4a
Brojen <brojen.bond@gmail.com>
2019-09-18 22:03:50 +0530

    Change path

--- src/app/pages/auth/auth-routing.module.ts
+++ src/app/pages/auth/auth-routing.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login.component';
+import { LoginComponent } from './login/login.component';
+import { AuthComponent } from './auth.component';
 
 const routes: Routes = [
-  {
-    path: '/login',
-    component: LoginComponent
-  },
+  // {
+  //   path: '/login',
+  //   component: LoginComponent
+  // },
   {
     path: '',
-    component: LoginComponent
+    component: AuthComponent,
+    children: [{
+      path: 'login',
+      component: LoginComponent
+    }]
   }
 ];
 
